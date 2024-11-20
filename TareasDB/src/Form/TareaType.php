0f5531c23466c01a0645ee4153d1da245e913de6
Fermin <ferminromero2005@gmail.com>
2024-11-20 16:55:52 +0100

    Add Tarea entity forms and templates for CRUD operations

---
+++ TareasDB/src/Form/TareaType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tarea;
+use App\Entity\Usuario;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TareaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titulo')
+            ->add('Usuario', EntityType::class, [
+                'class' => Usuario::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tarea::class,
+        ]);
+    }
+}
