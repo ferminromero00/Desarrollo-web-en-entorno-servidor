bcc269381572fc508a82e14b506ce4e1c1d2a711
Fermin <ferminromero2005@gmail.com>
2025-01-10 12:43:17 +0100

    Add functionality to manage articles in the shopping cart; update routes and templates for adding items

--- Amazon/src/Controller/CarritoController.php
+++ Amazon/src/Controller/CarritoController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Articulo;
+use App\Entity\LineaPedido;
 use App\Entity\Pedido;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CarritoController extends AbstractController
 {
-    #[Route('/carrito', name: 'app_carrito')]
+    #[Route('/articulos', name: 'app_carrito')]
     public function index(EntityManagerInterface $em, Request $request): Response
     {
         //Obtenemos todos los articulos
@@ -23,9 +24,30 @@ class CarritoController extends AbstractController
         $sesion = $request->getSession()->set('pedido', $pedido);
 
         return $this->render('carrito/index.html.twig', [
-            'articulos' => $articulos
+            'articulos' => $articulos,
+            'pedido' => $pedido
         ]);
     }
 
-    
+    #[Route('/carrito/nuevoarticulo/{id}', name: 'app_carrito_nuevoarticulo')]
+    public function addnuevoarticulo(EntityManagerInterface $em, Request $request, Articulo $articulo): Response
+    {
+        //Recuperamos el pedido
+        $pedido = $request->getSession()->get('pedido');
+        //Creamos nueva linea
+        $linea = new LineaPedido();
+        //Establecemos cantidad
+        $linea->setCantidad(1);
+        //Establecemos articulo
+        $linea->setArticulo($articulo);
+        //Añadimosla linea al pedido
+        $pedido->addLineaPedidos($linea);
+
+        return $this->render('carrito/nuevoarticulo.html.twig', [
+            'articulos' => $articulo,
+            
+        ]);
+    }
+
+
 }
