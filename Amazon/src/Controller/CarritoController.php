6fc447b3dd13a8815e9e3e64ecce8107be66fbb2
Fermin <ferminromero2005@gmail.com>
2025-01-10 12:12:51 +0100

    Enhance CarritoController to manage session-based orders; update index view to display article names and link to add items to the order

--- Amazon/src/Controller/CarritoController.php
+++ Amazon/src/Controller/CarritoController.php
@@ -3,20 +3,29 @@
 namespace App\Controller;
 
 use App\Entity\Articulo;
+use App\Entity\Pedido;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CarritoController extends AbstractController
 {
     #[Route('/carrito', name: 'app_carrito')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
+        //Obtenemos todos los articulos
         $articulos = $em->getRepository(Articulo::class)->findAll();
+        //Creamos un pedido
+        $pedido = new Pedido();
+        //Guardamos pedido en la sesion
+        $sesion = $request->getSession()->set('pedido', $pedido);
 
         return $this->render('carrito/index.html.twig', [
             'articulos' => $articulos
         ]);
     }
+
+    
 }
