5fad006fd2688e0d5cd5f363d5f0ba430b05e38e
Fermin <ferminromero2005@gmail.com>
2025-01-29 18:30:57 +0100

    Implement user registration and login functionality; add RegistrationController, SecurityController, and corresponding templates; update security configuration and form handling

--- MyWall/src/Entity/Usuario.php
+++ MyWall/src/Entity/Usuario.php
@@ -6,11 +6,13 @@ use App\Repository\UsuarioRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsuarioRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
