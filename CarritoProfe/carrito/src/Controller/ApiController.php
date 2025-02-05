656c14c4e0a8320ecf65b52183fa7733ce5e96b2
Fermin <ferminromero2005@gmail.com>
2025-02-05 14:28:03 +0100

    Remove commented-out code and streamline JSON response handling in ApiController

--- CarritoProfe/carrito/src/Controller/ApiController.php
+++ CarritoProfe/carrito/src/Controller/ApiController.php
@@ -46,19 +46,6 @@ final class ApiController extends AbstractController
 
         # Conbvertimos el array de arrays en un json
         return new JsonResponse(['articulos' => $arrayArt], JsonResponse::HTTP_OK);
-
-        # Se podría hacer con array_map
-        /*
-        $datos = array_map(function($articulo) {
-            
-            return
-            [
-                'nombre' => $articulo->getNombre(),                
-                'precio' => $articulo->getPrecio(),                
-            ];
-        }, $articulos);
-        return new JsonResponse(['articulos' => $datos], JsonResponse::HTTP_OK);
-        */
         
     }
 
@@ -79,59 +66,6 @@ final class ApiController extends AbstractController
         }, $pedidos);
 
         return new JsonResponse($datos);
-
-        /*
-        Si necesitamos las línea de artículo
-       
-        // Preparamos un arry donde ir gaurdando los objetos
-        $datos = [];
-
-        // Recorremos los pedidos
-        foreach ($pedidos as $pedido) {
-            // Obtenemos las líneas del pedido, pero como la consulta solo devuelve el pedido
-            // tenemos que consultar las líneas que pertenecen al pedido
-            // Tenemos que implementar en LineaPedidoRepository el método FindByPedido
-            $lineas = $em->getRepository(LineaPedido::class)->FindByPedido($pedido->getId())
-            
-            // Creamos un array donde guardar las líneas del pedido
-            $lineasJson = [];
-            // Recorremos las liíneas del pedido
-            foreach ($lineas as $linea) {
-                // Generamos un array asociativo con los datos del pedido
-                $lin = [
-                    'id' => $linea->getId(),
-                    'cantidad' => $linea->getCantidad(),
-                    // Y dentro,
-                    // otro array asociativo con los datos del producto
-                    'producto' => [
-                        'id' => $linea->getProducto()->getId(),
-                        'nombre' => $linea->getProducto()->getNombre(),
-                        'precio' => $linea->getProducto()->getPrecio()
-                    ]
-                ];
-                // Añadimos el array de la línea al array de de las líneas
-                array_push($lineasJson, $lin);
-            }
-            // Finalmente, creamos el pedido con sus datos
-            $ped = [
-                'id' => $pedido->getId(),
-                'fecha' => $pedido->getFecha(),
-                // Añadimos otro array con los datos del cliente
-                'usuario' => [
-                    'id' => $pedido->getCliente()->getId(),
-                    'nombre' => $pedido->getCliente()->getNombre(),
-                    
-                ],
-                // Añadimos las líneas creadas anteriormente
-                'lineas' => $lineasJson
-            ];
-            // Guardamos el pedido en el array de pedidos
-            array_push($datos, $ped);
-        }
-        // Lo enviamos con JSON
-        return $this->json($datos);
-
-        */
       
     }
     
