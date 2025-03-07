7ed1f6236d7fa9a3375317ae5de0226f3f527231
Fermin <ferminromero2005@gmail.com>
2025-03-07 11:01:29 +0100

    dasdas

--- examenbar/src/Controller/AppController.php
+++ examenbar/src/Controller/AppController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Caja;
+use App\Entity\Gasto;
+use App\Entity\Proveedor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,14 +21,57 @@ class AppController extends AbstractController
         $caja = $em->getRepository(Caja::class)->findAll();
 
         $arrayCaja = [];
-        
+        $total = 0;
+
         foreach ($caja as $a) {
             $e = ['id' => $a->getId(), 'fecha' => $a->getFecha(), 'cantidad' => $a->getCantidad()];
+            $total += $a->getCantidad();
             array_push($arrayCaja, $e);
         }
 
         return $this->render('inicio/caja.html.twig', [
-            'caja' => $arrayCaja
+            'caja' => $arrayCaja,
+            'total' => $total
+        ]);
+    }
+    #[Route('/compras', name: 'app_compras')]
+    public function compras(EntityManagerInterface $em): Response
+    {
+        $compras = $em->getRepository(Gasto::class)->findAll();
+        $total = 0;
+        $arrayCompras = [];
+
+        foreach ($compras as $a) {
+            $e = [
+                'id' => $a->getId(),
+                'fecha' => $a->getFecha(),
+                'cantidad' => $a->getCantidad(),
+                'proveedor' => $a->getProveedor(),
+                'factura' => $a->isFactura()
+            ];
+            $total += $a->getCantidad();
+            array_push($arrayCompras, $e);
+        }
+        return $this->render('inicio/compras.html.twig', [
+            'compras' => $arrayCompras,
+            'total' => $total
+        ]);
+    }
+    #[Route('/proveedores', name: 'app_proveedores')]
+    public function proveedores(EntityManagerInterface $em): Response
+    {
+        $compras = $em->getRepository(Proveedor::class)->findAll();
+        $arrayProveedores = [];
+
+        foreach ($compras as $a) {
+            $e = [
+                'id' => $a->getId(),
+                'nombre' => $a->getNombre()
+            ];
+            array_push($arrayProveedores, $e);
+        }
+        return $this->render('inicio/proveedores.html.twig', [
+            'proveedores' => $arrayProveedores,
         ]);
     }
 }
